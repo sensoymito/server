7b1f9ee8e1221d1ba564e909c2915836c4e69c49
YigitKaragol <itokaragol@gmail.com>
2026-07-01 09:03:10 +0900

    commit

---
+++ src/server/server.ts
@@ -0,0 +1,7 @@
+import express from "express"
+import http from "http"
+import { Server } from "socket.io"
+ 
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
